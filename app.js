aa226f03a64b039447a58494fa2afa34ffe8aafd
blu3fire <blu3fire89@gmail.com>
2021-06-24 14:19:08 +0800

    Add env, update mongodb & port

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-var express     = require("express");
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    passport    = require("passport"),
-    flash       = require("connect-flash"),
+require('dotenv').config()
+var express = require("express");
+app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    flash = require("connect-flash"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
-    Campground  = require("./models/campgrounds"),
-    User        = require("./models/user"),
-    Comment     = require("./models/comments"),
-    seedDB      = require("./seeds");
+    Campground = require("./models/campgrounds"),
+    User = require("./models/user"),
+    Comment = require("./models/comments"),
+    seedDB = require("./seeds");
 
-var commentRoutes       = require("./routes/comments"),
-    campgroundRoutes    = require("./routes/campgrounds"),
-    indexRoutes         = require("./routes/index");
+var commentRoutes = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes = require("./routes/index");
 
-//To fix all deprecation warnings
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useNewUrlParser', true);
 
-//connect to database
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_v13";
-mongoose.connect(url);
-//mongoose.connect("mongodb://localhost:27017/yelp_camp_v12");
-//mongoose.connect("mongodb+srv://blu3fire89:0089@cluster0.eygh2.mongodb.net/<dbname>?retryWrites=true&w=majority");
+// //connect to database
+var URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if (err) throw err;
+    console.log("Connected to MongoDB")
+})
 
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -48,7 +53,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.locals.currentUser = req.user;
     res.locals.error = req.flash("error");
     res.locals.success = req.flash("success");
@@ -57,10 +62,10 @@ app.use(function(req, res, next){
 
 app.use("/", indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-app.use( "/campgrounds", campgroundRoutes);
+app.use("/campgrounds", campgroundRoutes);
 
 //dynamic port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+    console.log(`Our app is running on port ${PORT}`);
 });
